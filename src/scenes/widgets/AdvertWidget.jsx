a42820f5295674d50203548a5d08dc929b01948a
Jaron-S <jaronschoor@gmail.com>
2023-02-14 18:56:41 -0700

    updated advert description

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -3,35 +3,39 @@ import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 
 const AdvertWidget = () => {
-    const { palette } = useTheme();
-    const dark = palette.neutral.dark;
-    const main = palette.neutral.main;
-    const medium = palette.neutral.medium;
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
 
-    return (
-        <WidgetWrapper>
-            <FlexBetween>
-                <Typography color={dark} variant="h5" fontWeight="500">
-                    Sponsored
-                </Typography>
-                <Typography color={medium}>Create Ad</Typography>
-            </FlexBetween>
-            <img
-                width="100%"
-                height="auto"
-                alt="advert"
-                src="https://social-media-app-server.onrender.com/assets/info4.jpeg"
-                style={{ borderRadius: "0.75rem", margin: "0.75rem 0"}}
-            />
-            <FlexBetween>
-                <Typography color={main}>MikaCosmetics</Typography>
-                <Typography color={medium}>mikacosmetics.com</Typography>
-            </FlexBetween>
-            <Typography color={medium} m="0.5rem 0">
-                Some random text about the product!
-            </Typography>
-        </WidgetWrapper>
-    )
-}
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="https://social-media-app-server.onrender.com/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>MikaCosmetics</Typography>
+        <Typography color={medium}>mikacosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Luminance: a range of high-quality and affordable beauty products
+        designed to enhance your natural beauty. From vibrant lipsticks to
+        nourishing skincare, our products are made with only the finest
+        ingredients to deliver exceptional results. Discover your perfect look
+        with Luminance by Mika Cosmetics.
+      </Typography>
+    </WidgetWrapper>
+  );
+};
 
 export default AdvertWidget;
