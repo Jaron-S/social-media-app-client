8c07080ef1944be6d3715c4ce2ef32b4112d7c1c
Jaron-S <jaronschoor@gmail.com>
2023-02-14 20:43:55 -0700

    shortened ad description

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -29,10 +29,8 @@ const AdvertWidget = () => {
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
         Luminance: a range of high-quality and affordable beauty products
-        designed to enhance your natural beauty. From vibrant lipsticks to
-        nourishing skincare, our products are made with only the finest
-        ingredients to deliver exceptional results. Discover your perfect look
-        with Luminance by Mika Cosmetics.
+        designed to enhance your natural beauty. Our products are made with only
+        the finest ingredients to deliver exceptional results.
       </Typography>
     </WidgetWrapper>
   );
