7895a2d163b156f8d57b3d192bdbeae23322fa57
Jaron-S <jaronschoor@gmail.com>
2023-02-21 10:54:47 -0700

    added client side photos for faster load time

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -1,17 +1,36 @@
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
-const UserImage = ({ image, size="60px" }) => {
-    return (
-        <Box width={size} height={size}>
-            <img
-                style={{ objectFit: "cover", borderRadius: "50%"}}
-                width={size}
-                height={size}
-                alt="user"
-                src={`https://social-media-app-server.onrender.com/assets/${image}`}
-            />
-        </Box>
-    );
+const UserImage = ({ image, size = "60px" }) => {
+  // Check if photos exists locally (for faster loading times)
+  function checkFileExists(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("HEAD", urlToFile, true);
+    xhr.send();
+
+    if (xhr.status === "404") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  // update post's picture path
+  if (!checkFileExists(image)) {
+    image = `https://social-media-app-server.onrender.com/assets/${image}`;
+  } else {
+    image = `/assets/${image}`;
+  }
+
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+        alt="user"
+        src={image}
+      />
+    </Box>
+  );
 };
 
 export default UserImage;
